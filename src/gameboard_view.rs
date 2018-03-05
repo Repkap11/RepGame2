69dfc3d73b46753e47da24e393d0c5cea8fe26ac
Paul Repka <repkap11@gmail.com>
2018-03-04 22:39:01 -0500

    Swithc to SDL backend so stuff isn't broken
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- src/gameboard_view.rs
+++ src/gameboard_view.rs
@@ -1,6 +1,7 @@
 //! Gameboard view.
 
 use graphics::types::Color;
+use graphics::character::CharacterCache;
 use graphics::{Context, Graphics};
 
 use GameboardController;
@@ -27,14 +28,18 @@ pub struct GameboardViewSettings {
     pub section_edge_radius: f64,
     /// Edge radius between cells.
     pub cell_edge_radius: f64,
+    /// Selected cell background color.
+    pub selected_cell_background_color: Color,
+    /// Text color.
+    pub text_color: Color,
 }
 
 impl GameboardViewSettings {
     /// Creates new gameboard view settings.
     pub fn new() -> GameboardViewSettings {
         GameboardViewSettings {
-            position: [10.0; 2],
-            size: 400.0,
+            position: [00.0, 00.0],
+            size: 450.0,
             background_color: [0.8, 0.8, 1.0, 1.0],
             border_color: [0.0, 0.0, 0.2, 1.0],
             board_edge_color: [0.0, 0.0, 0.2, 1.0],
@@ -43,6 +48,8 @@ impl GameboardViewSettings {
             board_edge_radius: 3.0,
             section_edge_radius: 2.0,
             cell_edge_radius: 1.0,
+            selected_cell_background_color: [0.9, 0.9, 1.0, 1.0],
+            text_color: [0.0, 0.0, 0.1, 1.0],
         }
     }
 }
@@ -56,61 +63,111 @@ pub struct GameboardView {
 impl GameboardView {
     /// Creates a new gameboard view.
     pub fn new(settings: GameboardViewSettings) -> GameboardView {
-        GameboardView {
-            settings: settings,
-        }
+        GameboardView { settings: settings }
     }
 
     /// Draw gameboard.
-    pub fn draw<G: Graphics>(&self, controller: &GameboardController, c: &Context, g: &mut G) {
-use graphics::{Line, Rectangle};
+    pub fn draw<G: Graphics, C>(&self, controller: &GameboardController, glyphs: &mut C, c: &Context, g: &mut G,) where
+        C: CharacterCache<Texture = G::Texture>,
+    {
+        use graphics::{Image, Line, Rectangle, Transformed};
 
-      let ref settings = self.settings;
-      let board_rect = [
-          settings.position[0], settings.position[1],
-          settings.size, settings.size,
-      ];
+        let ref settings = self.settings;
+        let board_rect = [
+            settings.position[0],
+            settings.position[1],
+            settings.size,
+            settings.size,
+        ];
+        print!("trans:{:?}\n", c.transform);
 
-      // Draw board background.
-      Rectangle::new(settings.background_color)
-          .draw(board_rect, &c.draw_state, c.transform, g);
+        // Draw board background.
+        Rectangle::new(settings.background_color).draw(board_rect, &c.draw_state, c.transform, g);
 
-      // Draw cell borders.
-      let cell_edge = Line::new(settings.cell_edge_color, settings.cell_edge_radius);
-      for i in 0..9 {
-          // Skip lines that are covered by sections.
-          if (i % 3) == 0 {continue;}
+        // Draw selected cell background.
+        if let Some(ind) = controller.selected_cell {
+            let cell_size = settings.size / 9.0;
+            let pos = [ind[0] as f64 * cell_size, ind[1] as f64 * cell_size];
+            let cell_rect = [
+                settings.position[0] + pos[0],
+                settings.position[1] + pos[1],
+                cell_size,
+                cell_size,
+            ];
+            Rectangle::new(settings.selected_cell_background_color).draw(
+                cell_rect,
+                &c.draw_state,
+                c.transform,
+                g,
+            );
+        }
 
-          let x = settings.position[0] + i as f64 / 9.0 * settings.size;
-          let y = settings.position[1] + i as f64 / 9.0 * settings.size;
-          let x2 = settings.position[0] + settings.size;
-          let y2 = settings.position[1] + settings.size;
+        // Draw characters.
+        let text_image = Image::new_color(settings.text_color);
+        let cell_size = settings.size / 9.0;
+        for j in 0..9 {
+            for i in 0..9 {
+                if let Some(ch) = controller.gameboard.char([i, j]) {
+                    let pos = [
+                        settings.position[0] + i as f64 * cell_size + 15.0,
+                        settings.position[1] + j as f64 * cell_size + 34.0,
+                    ];
+                    if let Ok(character) = glyphs.character(34, ch) {
+                        let ch_x = pos[0] + character.left();
+                        let ch_y = pos[1] - character.top();
+                        text_image.draw(
+                            character.texture,
+                            &c.draw_state,
+                            c.transform.trans(ch_x, ch_y),
+                            g,
+                        );
+                    }
+                }
+            }
+        }
 
-          let vline = [x, settings.position[1], x, y2];
-          cell_edge.draw(vline, &c.draw_state, c.transform, g);
+        // Draw cell borders.
+        let cell_edge = Line::new(settings.cell_edge_color, settings.cell_edge_radius);
+        for i in 0..9 {
+            // Skip lines that are covered by sections.
+            if (i % 3) == 0 {
+                continue;
+            }
 
-          let hline = [settings.position[0], y, x2, y];
-          cell_edge.draw(hline, &c.draw_state, c.transform, g);
-      }
+            let x = settings.position[0] + i as f64 / 9.0 * settings.size;
+            let y = settings.position[1] + i as f64 / 9.0 * settings.size;
+            let x2 = settings.position[0] + settings.size;
+            let y2 = settings.position[1] + settings.size;
 
-      // Draw section borders.
-      let section_edge = Line::new(settings.section_edge_color, settings.section_edge_radius);
-      for i in 0..3 {
-          // Set up coordinates.
-          let x = settings.position[0] + i as f64 / 3.0 * settings.size;
-          let y = settings.position[1] + i as f64 / 3.0 * settings.size;
-          let x2 = settings.position[0] + settings.size;
-          let y2 = settings.position[1] + settings.size;
+            let vline = [x, settings.position[1], x, y2];
+            cell_edge.draw(vline, &c.draw_state, c.transform, g);
 
-          let vline = [x, settings.position[1], x, y2];
-          section_edge.draw(vline, &c.draw_state, c.transform, g);
+            let hline = [settings.position[0], y, x2, y];
+            cell_edge.draw(hline, &c.draw_state, c.transform, g);
+        }
 
-          let hline = [settings.position[0], y, x2, y];
-          section_edge.draw(hline, &c.draw_state, c.transform, g);
-      }
+        // Draw section borders.
+        let section_edge = Line::new(settings.section_edge_color, settings.section_edge_radius);
+        for i in 0..3 {
+            // Set up coordinates.
+            let x = settings.position[0] + i as f64 / 3.0 * settings.size;
+            let y = settings.position[1] + i as f64 / 3.0 * settings.size;
+            let x2 = settings.position[0] + settings.size;
+            let y2 = settings.position[1] + settings.size;
 
-      // Draw board edge.
-      Rectangle::new_border(settings.board_edge_color, settings.board_edge_radius)
-          .draw(board_rect, &c.draw_state, c.transform, g);
-  }
+            let vline = [x, settings.position[1], x, y2];
+            section_edge.draw(vline, &c.draw_state, c.transform, g);
+
+            let hline = [settings.position[0], y, x2, y];
+            section_edge.draw(hline, &c.draw_state, c.transform, g);
+        }
+
+        // Draw board edge.
+        Rectangle::new_border(settings.board_edge_color, settings.board_edge_radius).draw(
+            board_rect,
+            &c.draw_state,
+            c.transform,
+            g,
+        );
+    }    
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 
 //! A RepGame2 game.
 
-extern crate piston;
 extern crate graphics;
-extern crate glutin_window;
 extern crate opengl_graphics;
+extern crate piston;
+extern crate sdl2_window;
+
 
 use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
-use glutin_window::GlutinWindow as Window;
+use piston::event_loop::{EventLoop, EventSettings, Events};
+use piston::input::RenderEvent;
+use sdl2_window::Sdl2Window;
+use opengl_graphics::{Filter, GlGraphics, GlyphCache, OpenGL, TextureSettings};
 
 pub use gameboard::Gameboard;
 pub use gameboard_controller::GameboardController;
@@ -20,51 +22,41 @@ mod gameboard;
 mod gameboard_controller;
 mod gameboard_view;
 
-use opengl_graphics::{ GlGraphics, OpenGL };
-
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
-
-    // Create an Glutin window.
-    let mut window: Window = WindowSettings::new(
-            "RepGame2",
-            [200, 200]
-        )
+    let settings = WindowSettings::new("RepGame2", [400; 2])
         .opengl(opengl)
         .exit_on_esc(true)
-        .build()
-        .unwrap();
+        .fullscreen(false)
+        .resizable(true)
+        .vsync(false);
+    let mut window: Sdl2Window = settings.build().expect("Could not create window");
 
-    let mut events = Events::new(EventSettings::new());
-    //let mut events = Events::new(EventSettings::new().lazy(true));
+    let mut events = Events::new(EventSettings::new().lazy(false));
     let mut gl = GlGraphics::new(opengl);
+
     let gameboard = Gameboard::new();
     let mut gameboard_controller = GameboardController::new(gameboard);
     let gameboard_view_settings = GameboardViewSettings::new();
     let gameboard_view = GameboardView::new(gameboard_view_settings);
 
-    // Using user input events, render and update
+    let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+    let ref mut glyphs = GlyphCache::new("assets/FiraSans-Regular.ttf", (), texture_settings)
+        .expect("Could not load font");
+
     while let Some(e) = events.next(&mut window) {
-        gameboard_controller.event(&e);
+        gameboard_controller.event(
+            gameboard_view.settings.position,
+            gameboard_view.settings.size,
+            &e,
+        );
         if let Some(args) = e.render_args() {
             gl.draw(args.viewport(), |c, g| {
-                graphics::clear([1.0; 4], g);
-                gameboard_view.draw(&gameboard_controller, &c, g);
+                use graphics::clear;
+
+                clear([1.0; 4], g);
+                gameboard_view.draw(&gameboard_controller, glyphs, &c, g);
             });
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
