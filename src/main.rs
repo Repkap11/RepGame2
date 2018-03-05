a94b16d055af739a43efdd6f14ce16bba312e349
Paul Repka <repkap11@gmail.com>
2018-03-04 21:06:36 -0500

    Renders test game screen
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,70 @@
-extern crate piston_window;
+#![deny(missing_docs)]
 
-use piston_window::*;
+//! A RepGame2 game.
+
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+
+pub use gameboard::Gameboard;
+pub use gameboard_controller::GameboardController;
+pub use gameboard_view::{GameboardView, GameboardViewSettings};
+
+mod gameboard;
+mod gameboard_controller;
+mod gameboard_view;
+
+use opengl_graphics::{ GlGraphics, OpenGL };
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new(
+            "RepGame2",
+            [200, 200]
+        )
+        .opengl(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-            rectangle(
-                [1.0, 0.0, 0.0, 1.0], // red
-                [0.0, 0.0, 100.0, 100.0],
-                context.transform,
-                graphics,
-            );
-        });
+
+    let mut events = Events::new(EventSettings::new());
+    //let mut events = Events::new(EventSettings::new().lazy(true));
+    let mut gl = GlGraphics::new(opengl);
+    let gameboard = Gameboard::new();
+    let mut gameboard_controller = GameboardController::new(gameboard);
+    let gameboard_view_settings = GameboardViewSettings::new();
+    let gameboard_view = GameboardView::new(gameboard_view_settings);
+
+    // Using user input events, render and update
+    while let Some(e) = events.next(&mut window) {
+        gameboard_controller.event(&e);
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                graphics::clear([1.0; 4], g);
+                gameboard_view.draw(&gameboard_controller, &c, g);
+            });
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
